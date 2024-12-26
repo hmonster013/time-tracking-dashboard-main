73bbdfe20d4ba7b533bb14b070aa6d4338a4b971
huytp <huytp@funtap.vn>
2024-12-26 18:05:23 +0700

    fix button

---
+++ script.js
@@ -0,0 +1,10 @@
+cardContentBody = document.getElementsByClassName("card__content--body");
+cardContentFooter = document.getElementsByClassName("card__content--footer");
+
+
+
+fetch('./data.json').then((respon) => {
+
+}).then((data) => {
+    
+});
